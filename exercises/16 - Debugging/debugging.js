b0d68c45eadeec5f40341e42b75eff2bcae08ca7
melissa160 <melissa160@gmail.com>
2020-04-09 13:07:39 -0500

    debugging notes

--- exercises/16 - Debugging/debugging.js
+++ exercises/16 - Debugging/debugging.js
@@ -9,18 +9,42 @@ people.forEach((person, index) => {
 });
 
 // Console Methods
+console.table(people)
+console.error(people[0].name)
+console.warn(people[1].name)
 
-// Callstack
+// // Tiene sentido dentro de una funcion, cuenta las veces que se envia el mismo string
+function doctorize1(name) {
+  console.count(`running Doctorize for ${name}`)
+  return `Dr. ${name}`;
+}
+
+// // Agrupar muchos console logs
+function doALotOffStuff(){
+  console.group('Doing many logs')
+  console.log('first')
+  console.log('second')
+  console.groupEnd('Doing many logs')
+}
+
+
+// Callstack or stacktrace
 
 // Grabbing Elements
+// // Desde el inspector para desarrolladores de chrome, seleccionar un elemento, ej un boton, luego7
+// // ir a la consola y poner $0 para que te retorne el elemento que habias seleccionado es un shorcut
+// // para seleccionar elementos. $1 seria el elemento anterior seleccionado y $2 el mas anterior y asi sucesivament
 
 // Breakpoints
+// // debugger
 
 // Scope
 
 // Network Requests
+// // Network tab in devtools chrome
 
 // Break On Attribute
+// // Se selecciona el elemento, click derecho Break on, nos permitiria ver desde que archivo js se llama la accion del elemento
 
 // Some Setup Code
 
